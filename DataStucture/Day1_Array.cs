b3d91ff369d7f517daec690fd00d6e80bd6e0df2
Hector Almonte <hbalmontess272@gmail.com>
2022-09-07 00:00:24 -0300

    Agregar archivos de proyecto.

---
+++ DataStucture/Day1_Array.cs
@@ -0,0 +1,44 @@
+﻿namespace DataStucture_I
+{
+    public class Solution_01
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> hSet = new HashSet<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (hSet.Contains(nums[i]))
+                {
+                    return true;
+                }
+                hSet.Add(nums[i]);
+            }
+            return false;
+        }
+    }
+
+    public class Solution_02
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int sum = 0;
+            int max = nums[0];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+
+                if (nums[i] > sum)
+                {
+                    sum = nums[i];
+                }
+                if (sum > max)
+                {
+                    max = sum;
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ DataStucture/Program.cs
@@ -0,0 +1,19 @@
+﻿using DataStucture_I;
+
+namespace DataStucture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day2_Array day2_Array = new Day2_Array();
+
+            int target = 9;
+            int[] nums = { 3, 2, 4 };
+
+            var result = day2_Array.TwoSum(nums, target);
+
+            Console.WriteLine(result);
+        }
+    }
+}

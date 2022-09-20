88d46993b81c792dfd6b24e63264c32705a1e787
Hector Almonte <hbalmontess272@gmail.com>
2022-09-20 18:18:36 -0300

    Recursion

---
+++ Recursion002/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fibonacci(5));
+        }
+
+        public static int Fibonacci(int n)
+        {
+            int result = 0;
+
+            // caso inductivo
+            if (n > 1)
+                result = Fibonacci(n - 1) + Fibonacci(n - 2);
+
+            // caso base
+            if (n <= 1)
+                result = 1;
+
+            return result;
+        }
+    }
+}
--- datastructure_course/Program.cs
+++ datastructure_course/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace System
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Factorial(5);
+
+            Console.WriteLine(result);
+        }
+
+        public static int Factorial(int n)
+        {
+            int result = 0;
+
+            // Caso inductivo
+            if(n > 1) result = n * Factorial(n - 1);
+
+            // Caso base
+            if (n == 1) result = 1;
+
+            return result;
+        }
+    }
+}
